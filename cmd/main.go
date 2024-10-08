0f97ba94f525831a1f6d5027f391c7c3b0de12d8
bogdanove <anoth3rx@gmail.com>
2024-10-08 09:06:01 +0300

    fix review comments

--- cmd/main.go
+++ cmd/main.go
@@ -6,36 +6,17 @@ import (
 	"log"
 	"net"
 
-	"github.com/bogdanove/chat-server/pkg/chat_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
+
+	"github.com/bogdanove/chat-server/pkg/chat_v1"
 )
 
 const grpcPort = 50051
 
 type server struct {
 	chat_v1.UnimplementedChatV1Server
-}
-
-func (s *server) Create(ctx context.Context, req *chat_v1.CreateRequest) (*chat_v1.CreateResponse, error) {
-	log.Printf("create new chat with usernames: %v", req.Usernames)
-	_ = ctx
-	return &chat_v1.CreateResponse{
-		Id: int64(len(req.Usernames)),
-	}, nil
-}
-
-func (s *server) Delete(ctx context.Context, req *chat_v1.DeleteRequest) (*emptypb.Empty, error) {
-	log.Printf("delete chat for id: %d", req.Id)
-	_ = ctx
-	return new(emptypb.Empty), nil
-}
-
-func (s *server) SendMessage(ctx context.Context, req *chat_v1.SendMessageRequest) (*emptypb.Empty, error) {
-	log.Printf("incoming message received: %+v", req.Message)
-	_ = ctx
-	return new(emptypb.Empty), nil
 }
 
 func main() {
@@ -45,7 +26,9 @@ func main() {
 	}
 
 	s := grpc.NewServer()
+
 	reflection.Register(s)
+
 	chat_v1.RegisterChatV1Server(s, &server{})
 
 	log.Printf("server listening at: %v", listen.Addr())
@@ -53,4 +36,27 @@ func main() {
 	if err = s.Serve(listen); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+// Create - создания нового чата
+func (s *server) Create(_ context.Context, req *chat_v1.CreateRequest) (*chat_v1.CreateResponse, error) {
+	log.Printf("create new chat with title: %v", req.ChatTitle)
+
+	return &chat_v1.CreateResponse{
+		Id: int64(len(req.Ids)),
+	}, nil
+}
+
+// Delete - удаление чата из системы по его идентификатору
+func (s *server) Delete(_ context.Context, req *chat_v1.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("delete chat for id: %d", req.Id)
+
+	return &emptypb.Empty{}, nil
+}
+
+// SendMessage - отправка сообщения на сервер
+func (s *server) SendMessage(_ context.Context, req *chat_v1.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("incoming message received: %+v", req.Message)
+
+	return &emptypb.Empty{}, nil
 }
