2c88733030ea21cbd289e2d37efb4764f7bfdbc9
bogdanove <anoth3rx@gmail.com>
2024-10-09 23:45:24 +0300

    add pg

--- cmd/main.go
+++ cmd/main.go
@@ -2,33 +2,65 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"flag"
 	"log"
 	"net"
 
+	sq "github.com/Masterminds/squirrel"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
 
+	"github.com/bogdanove/chat-server/internal/config"
+	"github.com/bogdanove/chat-server/internal/config/env"
 	"github.com/bogdanove/chat-server/pkg/chat_v1"
 )
 
-const grpcPort = 50051
+var configPath string
+
+func init() {
+	flag.StringVar(&configPath, "config-path", ".env", "path to config file")
+}
 
 type server struct {
 	chat_v1.UnimplementedChatV1Server
 }
 
 func main() {
-	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	flag.Parse()
+	ctx := context.Background()
+
+	// Считываем переменные окружения
+	err := config.Load(configPath)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	grpcConfig, err := env.NewGRPCConfig()
+	if err != nil {
+		log.Fatalf("failed to get grpc config: %v", err)
+	}
+
+	pgConfig, err := env.NewPGConfig()
+	if err != nil {
+		log.Fatalf("failed to get pg config: %v", err)
+	}
+
+	listen, err := net.Listen("tcp", grpcConfig.Address())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	// Создаем пул соединений с базой данных
+	pool, err := pgxpool.Connect(ctx, pgConfig.DSN())
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer pool.Close()
+
 	s := grpc.NewServer()
-
 	reflection.Register(s)
-
 	chat_v1.RegisterChatV1Server(s, &server{})
 
 	log.Printf("server listening at: %v", listen.Addr())
@@ -41,6 +73,8 @@ func main() {
 // Create - создания нового чата
 func (s *server) Create(_ context.Context, req *chat_v1.CreateRequest) (*chat_v1.CreateResponse, error) {
 	log.Printf("create new chat with title: %v", req.ChatTitle)
+
+	_ = sq.InsertBuilder{}
 
 	return &chat_v1.CreateResponse{
 		Id: int64(len(req.Ids)),
---
+++ migration_prod.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source prod.env
+
+MIGRATION_DIR=$(echo "$MIGRATION_DIR" | tr -d '[:space:]')
+
+sleep 2 && goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up -v
